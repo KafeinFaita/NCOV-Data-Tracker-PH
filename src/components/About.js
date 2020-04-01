e5b5c10b966908d4b75fe3552cd9bc51499a12e4
KafeinFaita <lynyrd.andres47@gmail.com>
2020-04-01 20:13:45 +0800

    updated credits

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,11 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>App made by: LYNYRD ANDRES</h1>
-            <p>This App is still under construction</p>
+            <h2 style={{margin: '20px 0'}}>App Creators:</h2>
+            <p>LYNYRD ANDRES and RAVELOS CAKES</p>
+
+            <h2 style={{margin: '50px 0', color: 'red'}}>THIS APP IS STILL UNDER CONSTRUCTION</h2>
+
         </div>
 
         
